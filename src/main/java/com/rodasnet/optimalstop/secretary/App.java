c8d820a69af0fdceae8e90e54ccbd483903af784
Daniel Rodas <daniel.rodas1@hotmail.com>
2021-03-03 14:41:45 -0800

    Add Candidate entity

---
+++ src/main/java/com/rodasnet/optimalstop/secretary/App.java
@@ -0,0 +1,34 @@
+package com.rodasnet.optimalstop.secretary;
+
+import java.util.*; 
+import java.util.stream.Collectors; 
+import java.util.stream.Stream;
+import java.util.Arrays;
+
+import com.jongsoft.lang.collection.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args )
+    {
+        List<Candidate> candies = Arrays.asList(
+            new Candidate("Bob",1,1,1), 
+            new Candidate("Alice",1,1,1), 
+            new Candidate("Lisa",1,1,1),
+            new Candidate("John",1,1,1),
+            new Candidate("Ema",1,1,1),
+            new Candidate("Chris",1,1,1),
+            new Candidate("Nancy",1,1,1),
+            new Candidate("Eric",1,1,1)
+        );
+
+        for (int i = 0; i < candies.size(); i++) {
+            String message = "Hello, candidate " + candies.get(i).getName() + "!";
+            System.out.println(message);
+        }        
+    }
+}
